89e9865b597dc0ecb1f3181617832e5ae3ece518
juli <julik0902@gmail.com>
2021-05-14 10:48:40 +0530

    to be complete

---
+++ day_6/defining_function.py
@@ -0,0 +1,13 @@
+#defining function
+def my_function():
+  print("hey")
+  print("bye")
+#calling function
+my_function()
+#intentation 
+def my_functions():
+    print("hey")
+    a = 3
+    if a > 2:
+        print("hey there!")
+my_functions()
